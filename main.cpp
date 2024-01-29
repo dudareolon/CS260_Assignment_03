518927ee23f93e16544fec87faf22ba1d355fd2a
Eduarda (Duda) Reolon <duda.reolon.1589@mail.linnbenton.edu>
2024-01-28 21:00:58 -0800

    testing section organized

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 // Description: This code uses a linked list to implement the queue abstract data type (First in, First Out data type). 
 // This program has methods to enqueue (add a value to the rear of the queue), dequeue (remove a value from the front of the queue),
 // and peek (check what is the value at the rear of the queue without deleting it). Lastly, this program also runs tests to make sure
-// the classes are correctly functioning.
+// the class is correctly functioning.
 
 
 #include <iostream>
@@ -134,46 +134,89 @@ class Queue {
 
 // Testing
 int main() {
-    Queue myQueue;
-    myQueue.initialize();
+    Queue Testing_Queue; // create a queue from the class
+    Testing_Queue.initialize(); //must initialize queue before running the functions
 
-    myQueue.back_peek();
-    myQueue.dequeue();
-    myQueue.front_peek();
-    myQueue.enqueue(10);
-    myQueue.dequeue();
-    myQueue.dequeue();
+    // Test 1
+    // Try to run dequeue, back and front peek when the queue is empty, no nodes have been added yet
+    Testing_Queue.back_peek();
+    Testing_Queue.dequeue();
+    Testing_Queue.front_peek();
+    // Output:
+    // Can't peek what the rear value of the queue is because the queue is empty
+    // Can't remove a value from the queue because the queue is empty
+    // Can't peek what the front value of the queue is because the queue is empty
+    // Correct
 
-    myQueue.enqueue(20);
-    myQueue.enqueue(30);
-    myQueue.enqueue(5);
-    myQueue.enqueue(4);
-    myQueue.enqueue(3);
-    myQueue.enqueue(0);
+    // Test 2
+    // Add the first variable to the queue (so there is only one node in the queue) and then peek what are the front and rear variables
+    Testing_Queue.enqueue(10);
+    Testing_Queue.front_peek();
+    Testing_Queue.back_peek();
+    // Output:
+    // 10 was added to the queue
+    // The number at the front of the queue is 10
+    // The number in the end of the queue is 10
+    // Correct
 
-    myQueue.front_peek();
+    // Test 3 
+    // Queue has only one variable, but attempt to remove two variables out of the queue
+    Testing_Queue.dequeue();
+    Testing_Queue.dequeue();
+    // Output:
+    // 10 was removed from the queue
+    // Can't remove a value from the queue because the queue is empty
+    // Correct
 
-    myQueue.dequeue();
+    // Test 4
+    // Add several nodes to the queue and check if the front and rear pointers are pointing to the correct node
+    Testing_Queue.enqueue(20);
+    Testing_Queue.enqueue(30);
+    Testing_Queue.enqueue(5);
+    Testing_Queue.enqueue(4);
+    Testing_Queue.enqueue(3);
+    Testing_Queue.enqueue(0);
+    Testing_Queue.front_peek();
+    Testing_Queue.back_peek();
+    // Front should be pointing to the node 20 and rear should be pointing to 0
+    // Output:
+    // The number at the front of the queue is 20
+    // The number in the end of the queue is 0
+    // Correct
 
-    myQueue.back_peek();
+    // Test 4
+    // Check if the dequeue function is following the FIFO principle, and removing the node on the front which would be 20  
+    Testing_Queue.dequeue();
+    // Output:
+    // 20 was removed from the queue
+    // Correct
 
-    myQueue.front_peek();
+    // Test 5
+    // Check if after a variable was removed that the front pointer would adjust and if the rear pointer would remain pointing to the last variable in
+    Testing_Queue.front_peek();
+    Testing_Queue.back_peek();
+    // Output:
+    // The number at the front of the queue is 30 (updated to the variable added right after 20 was added)
+    // The number in the end of the queue is 0 (remained the same)
+    // Correct
 
-    myQueue.dequeue();
-
-    myQueue.front_peek();
-
-    myQueue.dequeue();
-
-    myQueue.front_peek();
-
-    myQueue.back_peek();
-
-    myQueue.dequeue();
-    myQueue.dequeue();
-    myQueue.dequeue();
-    myQueue.dequeue();
-    myQueue.back_peek();
+    // Test 6
+    // Remove more variables than there are in the queue and then try to check for the value the rear pointer is indicating
+    // Check if rear pointer is updating when the queue gets empty
+    Testing_Queue.dequeue();
+    Testing_Queue.dequeue();
+    Testing_Queue.dequeue();
+    Testing_Queue.dequeue();
+    Testing_Queue.dequeue();
+    Testing_Queue.dequeue();
+    Testing_Queue.dequeue();
+    Testing_Queue.back_peek();
+    // Output:
+    // 0 was removed from the queue (last number from the queue is removed)
+    // Can't remove a value from the queue because the queue is empty (error message)
+    // Can't remove a value from the queue because the queue is empty (error message)
+    // Can't peek what the rear value of the queue is because the queue is empty (error message)
+    // Correct
 
     return 0;
 }
